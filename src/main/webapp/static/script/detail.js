b5e6c1d2f277905119b8e7b21e2d7d49a0ba4d02
Huang Yulong <2450530918@qq.com>
2022-03-23 11:17:15 +0800

    make the overflow text be control of button

---
+++ src/main/webapp/static/script/detail.js
@@ -0,0 +1,38 @@
+$(function (){
+    // 如果文本超出框外就显示“显示”
+    (function (){
+        let $textP = $(".inf-right>li>p");
+        // console.log($textP);
+        // console.log($textP[0]);
+        // console.log($($textP[0]));
+        let $textS = $(".inf-right>li>span");
+        let i;
+        let thisHeight;
+        // 获取rem值
+        let rem = parseInt(window.getComputedStyle(document.documentElement)["fontSize"]);
+        // 遍历所有文本 检测是否有长度超标的
+        for(i = 0; i < $textP.length; i++){
+            thisHeight = $($textP[i]).height();
+            // wordWidth = $textP[i].scrollHeight;
+            if(thisHeight > 0.9 * rem){
+                $($textP[i]).css('height','0.9rem');
+                $($textS[i]).css('display','block');
+                $($textS[i]).click(function (){
+                    if($(this).text() ==='显示'){
+                        // console.log(i);
+                        // console.log(thisHeight);
+                        // $($textP[i]).css('height',);
+                        let $pre = $(this).prev();
+                        $pre.css('height',$pre[0].scrollHeight);
+                        // console.log($pre[0].scrollHeight);
+                        $(this).text('隐藏');
+                    }else{
+                        let $pre = $(this).prev();
+                        $($pre).css('height','0.9rem');
+                        $(this).text('显示');
+                    }
+                })
+            }
+        }
+    }());
+})
